1477add805a08d64f37ff2069d94490938de2d11
disxo-cloud <lieghton.gonzalo@outlook.com>
2020-07-10 22:25:27 -0300

    s

--- 1-NanoJack.py
+++ 1-NanoJack.py
@@ -7,12 +7,20 @@ def deck_of_cards(n):
     for i in range(n):
         deck.append(random.randint(1,14))
     return deck
-
-
-
-
+#%%
+def play(m):
+    sum = 0
+    while (sum <= 21 and m != []):
+        sum = sum + m.pop(0)
+    return m
 
 #%%
-n=int(input("Insert a number: "))
-print(deck_of_cards(n))
-#%%
+n=int(input("Insert the number of cards in your deck: "))
+
+n=deck_of_cards(n)
+
+print(n)
+print(play(n))
+print(n)
+
+# %%
