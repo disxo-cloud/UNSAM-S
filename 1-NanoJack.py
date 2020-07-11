ef0710e5d259ef3c90bfb79e31751b6b21190f9d
disxo-cloud <lieghton.gonzalo@outlook.com>
2020-07-10 22:09:27 -0300

    1

--- 1-NanoJack.py
+++ 1-NanoJack.py
@@ -1,13 +1,18 @@
 import random
-#%%
 
 def deck_of_cards(n):
+    if n is str:
+        raise TypeError("Please input an integer")
     deck = []
     for i in range(n):
         deck.append(random.randint(1,14))
     return deck
 
 
-print(deck_of_cards(10))
-print("Hello")
-# %%
+
+
+
+#%%
+n=int(input("Insert a number: "))
+print(deck_of_cards(n))
+#%%
