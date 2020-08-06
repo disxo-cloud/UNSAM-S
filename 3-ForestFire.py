388d93317eb9f90ba01c0848610ded282cf8a1fd
disxo-cloud <lieghton.gonzalo@outlook.com>
2020-08-05 22:46:12 -0300

    0.3

--- 3-ForestFire.py
+++ 3-ForestFire.py
@@ -3,29 +3,30 @@ import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.animation as animation
 import numpy as np
 import random
+import math
 
-def gen_bosque(n):
+def generate_forest(n):
     arr = []
     for i in range(n):
         arr.append(i)
     xpos = np.repeat(arr, n)
     ypos = arr * n 
-    zpos = [0] * (n * 4)
-    dx = [1] * (n * 4)
-    dy = [1] * (n * 4)
-    dz = [0] * (n * 4)
+    zpos = [0] * (n * n)
+    dx = [1] * (n * n)
+    dy = [1] * (n * n)
+    dz = [0] * (n * n)
     return xpos,ypos,zpos,dx,dy,dz
 
 #%%
 def update_bars(num, bars):
     a = random.randint(0,1)
     if a == 0:        
-        i = random.randint(0, 16)
+        i = random.randint(0, len(dx))
         dz[i] += 1
         bars[i] = ax.bar3d(xpos[i], ypos[i], zpos[i], dx[i], dy[i], dz[i], color=random.choice(['g']))
         return bars
     if a == 1:        
-        i = random.randint(0, 16)
+        i = random.randint(0, len(dx))
         dz[i] = -1
         bars[i] = ax.bar3d(xpos[i], ypos[i], zpos[i], dx[i], dy[i], dz[i], color=random.choice(['r']))
         return bars
@@ -33,12 +34,12 @@ def update_bars(num, bars):
 def update_bars0(num, bars):
     a = random.randint(0,1)
     if a == 0:        
-        i = random.randint(0, 16)
+        i = random.randint(0, len(dx))
         dz[i] += 5
         bars[i] = ax.bar3d(xpos[i], ypos[i], zpos[i], dx[i], dy[i], dz[i], color=random.choice(['g']))
         return bars
     if a == 1:        
-        i = random.randint(0, 16)
+        i = random.randint(0, len(dx))
         dz[i] = -1
         bars[i] = ax.bar3d(xpos[i], ypos[i], zpos[i], dx[i], dy[i], dz[i], color=random.choice(['r']))
         return bars
@@ -56,19 +57,21 @@ fig = plt.figure()
 ax = p3.Axes3D(fig)
 
 
-xpos = (gen_bosque(n))[0]
-ypos = (gen_bosque(n))[1]
-zpos = (gen_bosque(n))[2]
-dx = (gen_bosque(n))[3]
-dy = (gen_bosque(n))[4]
-dz = (gen_bosque(n))[5]
+xpos = (generate_forest(n))[0]
+ypos = (generate_forest(n))[1]
+zpos = (generate_forest(n))[2]
+dx = (generate_forest(n))[3]
+dy = (generate_forest(n))[4]
+dz = (generate_forest(n))[5]
 
+print(dx)
+print(dy)
 
-
+a = n*n 
 
 # add bars
 bars = []
-for i in range(16):
+for i in range(len(dx)):
     bars.append(ax.bar3d(xpos[i], ypos[i], zpos[i], dx[i], dy[i], dz[i], color=['g']))
 ax.set_title('Beta Forestal')
 
