c3f62b863bb83a86eac80fc3bc70fc688c9fb272
disxo-cloud <lieghton.gonzalo@outlook.com>
2020-08-05 21:13:39 -0300

    0.1

--- 3-ForestFire.py
+++ 3-ForestFire.py
@@ -1,13 +1,36 @@
-import plotly.graph_objects as go
-import pandas as pd
-import numpy as np
-# Read data from a csv
-z_data = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/api_docs/mt_bruno_elevation.csv')
-z = z_data.values
-sh_0, sh_1 = z.shape
-x, y = np.linspace(0, 1, sh_0), np.linspace(0, 1, sh_1)
-fig = go.Figure(data=[go.Surface(z=z, x=x, y=y)])
-fig.update_layout(title='Mt Bruno Elevation', autosize=False,
-                  width=500, height=500,
-                  margin=dict(l=65, r=50, b=65, t=90))
-fig.show()
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.axes3d as p3
+import matplotlib.animation as animation
+import random
+
+def update_bars(num, bars):
+    a = random.randint(0,1)
+    if a == 0:        
+        i = random.randint(0, 16)
+        dz[i] = 1
+        bars[i] = ax.bar3d(xpos[i], ypos[i], zpos[i], dx[i], dy[i], dz[i], color=random.choice(['g']))
+        return bars
+    if a == 1:        
+        i = random.randint(0, 16)
+        dz[i] = -1
+        bars[i] = ax.bar3d(xpos[i], ypos[i], zpos[i], dx[i], dy[i], dz[i], color=random.choice(['r']))
+        return bars
+
+
+fig = plt.figure()
+ax = p3.Axes3D(fig)
+xpos = [0,0,0,0,1,1,1,1,2,2,2,2,3,3,3,3]
+ypos = [0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3]
+zpos = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+dx =   [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+dy =   [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+dz =   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+  
+# add bars
+bars = []
+for i in range(16):
+    bars.append(ax.bar3d(xpos[i], ypos[i], zpos[i], dx[i], dy[i], dz[i], color=['g']))
+ax.set_title('Beta Forestal')
+
+line_ani = animation.FuncAnimation(fig, update_bars, 20, fargs=[bars], interval=100, blit=False)
+plt.show()
